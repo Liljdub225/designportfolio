0cd8be51bdbf62d79c1771bd90d3292e4e2ecf5e
Liljdub225 <joshweigt99@gmail.com>
2025-09-21 20:25:18 -0500

    added navbar fixes and back to top buttons on most of the project pages

--- App/projects.js
+++ App/projects.js
@@ -1,13 +1,14 @@
-const hamburger = document.querySelector(".hamburger") //both of these are getting access to these elements
+const hamburger = document.querySelector(".hamburger") // both of these are getting access to these elements
 const navbarLinks = document.querySelector(".navbarLinks")
-const backToTop = document.getElementById("backToTop"); // ids do not need period, while classes do
+const backToTop = document.getElementById("backToTop"); // targerting id for backtotop
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navbarLinks.classList.toggle("active");
 })
 
-// Show button when user scrolls down 300px
+
+  // Show button when user scrolls down 300px
   window.onscroll = () => {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) { // need both because browsers are weird
       backToTop.style.display = "block";
